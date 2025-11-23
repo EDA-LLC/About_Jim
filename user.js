4e49608719d94dcbdc870283487dfcc26d8de830
EDA-LLC <jimengandela@gmail.com>
2025-11-23 11:05:26 -0600

    Add files via upload
    Initial upload of website

---
+++ user.js
@@ -0,0 +1,15 @@
+/*  
+    User JavaScript template for sites exported by MyInfo.
+    This file will be inserted in each page of the web site.
+ 
+    Usage:
+    You can detect which page is active (where user.js is currently inserted) by checking for:
+        "tree-page": the main page of the site (index.html). Contains the tree and the content frame for the documents
+        "document-page": the page for the currently active document shown in the content frame
+        "welcome-page": the welcome page (welcome.html) of the site is shown, if there is no text in the root document of the exported topic
+*/
+
+collapseAllRows = function() 
+{
+
+}
